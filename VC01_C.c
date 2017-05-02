b363a5ef6a1489535d9f47ed9098f9258349580e
dingfude <dingfude@dingfudedeMacBook-Pro-2.local>
2017-05-02 15:46:32 +0800

    【Update】5-2 创建桥接文件和C文件

---
+++ VC01_C.c
@@ -0,0 +1,10 @@
+//
+//  VC01_C.c
+//  算法Test
+//
+//  Created by DFD on 2017/5/2.
+//  Copyright © 2017年 DFD. All rights reserved.
+//
+
+#include "VC01_C.h"
+
---
+++ VC01_C.h
@@ -0,0 +1,14 @@
+//
+//  VC01_C.h
+//  算法Test
+//
+//  Created by DFD on 2017/5/2.
+//  Copyright © 2017年 DFD. All rights reserved.
+//
+
+#ifndef VC01_C_h
+#define VC01_C_h
+
+#include <stdio.h>
+
+#endif /* VC01_C_h */
